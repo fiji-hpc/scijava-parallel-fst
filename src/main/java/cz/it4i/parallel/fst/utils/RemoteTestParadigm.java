40a5a51eb0948aefeb1edb0ce6ef88f93fe84014
Jan Kožusznik <jan@kozusznik.cz>
2019-07-11 14:22:37 +0200

    feat: RemoteTestParadigm support MultipleHostaParadigm

--- src/main/java/cz/it4i/parallel/fst/utils/RemoteTestParadigm.java
+++ src/main/java/cz/it4i/parallel/fst/utils/RemoteTestParadigm.java
@@ -1,25 +1,31 @@
 package cz.it4i.parallel.fst.utils;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.scijava.Context;
 import org.scijava.parallel.ParallelizationParadigm;
 
+import cz.it4i.parallel.Host;
+import cz.it4i.parallel.MultipleHostParadigm;
 import cz.it4i.parallel.RunningRemoteServer;
 import cz.it4i.parallel.runners.ServerRunner;
 import cz.it4i.parallel.utils.TestParadigm;
 
 public class RemoteTestParadigm extends TestParadigm implements
-	RunningRemoteServer
+	RunningRemoteServer, MultipleHostParadigm
 {
 
 	private ServerRunner server;
+	private ParallelizationParadigm paradigm;
 
 	public RemoteTestParadigm(ServerRunner runner,
 		ParallelizationParadigm paradigm)
 	{
 		super(runner, paradigm);
 		this.server = runner;
+		this.paradigm = paradigm;
 	}
 
 	public RemoteTestParadigm(ServerRunner runner, Context context) {
@@ -40,6 +46,36 @@ public class RemoteTestParadigm extends TestParadigm implements
 	@Override
 	public List<Integer> getNCores() {
 		return server.getNCores();
+	}
+
+	@Override
+	public void setHosts(Collection<Host> hosts) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<String> getHosts() {
+
+		if (paradigm instanceof MultipleHostParadigm) {
+			MultipleHostParadigm multipleHostParadigm =
+				(MultipleHostParadigm) paradigm;
+			return multipleHostParadigm.getHosts();
+		}
+		throw new UnsupportedOperationException("paradigm: " + paradigm.getClass() +
+			" is not " + MultipleHostParadigm.class.getSimpleName());
+	}
+
+	@Override
+	public List<Map<String, Object>> runOnHosts(String commandName,
+		Map<String, Object> parameters, List<String> hosts)
+	{
+		if (paradigm instanceof MultipleHostParadigm) {
+			MultipleHostParadigm multipleHostParadigm =
+				(MultipleHostParadigm) paradigm;
+			return multipleHostParadigm.runOnHosts(commandName, parameters, hosts);
+		}
+		throw new UnsupportedOperationException("paradigm: " + paradigm.getClass() +
+			" is not " + MultipleHostParadigm.class.getSimpleName());
 	}
 
 }
