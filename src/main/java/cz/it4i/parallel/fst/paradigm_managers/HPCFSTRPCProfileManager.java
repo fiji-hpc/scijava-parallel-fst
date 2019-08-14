3bfd8a4f68862205a48aeba6bdb6eb5337a17d67
Jan Kožusznik <jan@kozusznik.cz>
2019-08-14 13:16:09 +0200

    feat: Allow work with scijava-parallel:8.0.0-SNAPSHOT

--- src/main/java/cz/it4i/parallel/fst/paradigm_managers/HPCFSTRPCProfileManager.java
+++ src/main/java/cz/it4i/parallel/fst/paradigm_managers/HPCFSTRPCProfileManager.java
@@ -1,7 +1,5 @@
 package cz.it4i.parallel.fst.paradigm_managers;
 
-import java.util.Map;
-
 import org.scijava.Context;
 import org.scijava.parallel.ParadigmManager;
 import org.scijava.plugin.Parameter;
@@ -11,7 +9,6 @@ import cz.it4i.parallel.fst.FSTRPCParadigm;
 import cz.it4i.parallel.fst.runners.HPCFSTRPCServerRunnerUI;
 import cz.it4i.parallel.runners.HPCSettings;
 import cz.it4i.parallel.runners.MultipleHostsParadigmManagerUsingRunner;
-import cz.it4i.parallel.ui.HPCSettingsGui;
 
 @Plugin(type = ParadigmManager.class)
 public class HPCFSTRPCProfileManager extends
@@ -34,17 +31,5 @@ public class HPCFSTRPCProfileManager extends
 	@Override
 	public String toString() {
 		return "FSTRPC on HPC";
-	}
-
-	@Override
-	protected HPCSettings doEdit(Map<String, Object> inputs) {
-		return HPCSettingsGui.showDialog(ctx, inputs);
-	}
-
-	@Override
-	protected void fillInputs(HPCSettings settings,
-		Map<String, Object> inputs)
-	{
-		HPCSettingsGui.fillInputs(settings, inputs);
 	}
 }
--- src/main/java/cz/it4i/parallel/fst/paradigm_managers/LocalFSTRPCProfileManager.java
+++ src/main/java/cz/it4i/parallel/fst/paradigm_managers/LocalFSTRPCProfileManager.java
@@ -1,7 +1,5 @@
 package cz.it4i.parallel.fst.paradigm_managers;
 
-import java.util.Map;
-
 import org.scijava.Context;
 import org.scijava.parallel.ParadigmManager;
 import org.scijava.plugin.Parameter;
@@ -11,7 +9,6 @@ import cz.it4i.parallel.fst.FSTRPCParadigm;
 import cz.it4i.parallel.fst.runners.FSTRPCServerRunner;
 import cz.it4i.parallel.runners.ImageJServerRunnerSettings;
 import cz.it4i.parallel.runners.MultipleHostsParadigmManagerUsingRunner;
-import cz.it4i.parallel.ui.ImageJSettingsGui;
 
 @Plugin(type = ParadigmManager.class)
 public class LocalFSTRPCProfileManager extends
@@ -36,15 +33,4 @@ public class LocalFSTRPCProfileManager extends
 		return "Local FSTRPC";
 	}
 
-	@Override
-	protected ImageJServerRunnerSettings doEdit(Map<String, Object> inputs) {
-		return ImageJSettingsGui.showDialog(ctx, inputs);
-	}
-
-	@Override
-	protected void fillInputs(ImageJServerRunnerSettings settings,
-		Map<String, Object> inputs)
-	{
-		ImageJSettingsGui.fillInputs(settings, inputs);
-	}
 }
