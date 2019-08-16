1a575178007937cd20420df7f3611c04fb26ee51
Jan Kožusznik <jan@kozusznik.cz>
2019-08-16 16:20:36 +0200

    fix: Ensure modality of runner status window.

--- src/main/java/cz/it4i/parallel/fst/paradigm_managers/HPCFSTRPCProfileManager.java
+++ src/main/java/cz/it4i/parallel/fst/paradigm_managers/HPCFSTRPCProfileManager.java
@@ -9,14 +9,31 @@ import cz.it4i.parallel.fst.FSTRPCParadigm;
 import cz.it4i.parallel.fst.runners.HPCFSTRPCServerRunnerUI;
 import cz.it4i.parallel.runners.HPCSettings;
 import cz.it4i.parallel.runners.MultipleHostsParadigmManagerUsingRunner;
+import cz.it4i.parallel.runners.ServerRunner;
+import cz.it4i.parallel.ui.HPCImageJServerRunnerWithUI;
+import cz.it4i.parallel.ui.HavingOwnerWindow;
+import javafx.stage.Window;
 
 @Plugin(type = ParadigmManager.class)
 public class HPCFSTRPCProfileManager extends
 	MultipleHostsParadigmManagerUsingRunner<FSTRPCParadigm, HPCSettings>
+	implements HavingOwnerWindow<Window>
 {
 
 	@Parameter
 	private Context ctx;
+
+	private Window ownerWindow;
+
+	@Override
+	public Class<Window> getType() {
+		return Window.class;
+	}
+
+	@Override
+	public void setOwner(Window parent) {
+		ownerWindow = parent;
+	}
 
 	@Override
 	public Class<FSTRPCParadigm> getSupportedParadigmType() {
@@ -31,5 +48,12 @@ public class HPCFSTRPCProfileManager extends
 	@Override
 	public String toString() {
 		return "FSTRPC on HPC";
+	}
+
+	@Override
+	protected void initRunner(ServerRunner<?> runner) {
+		HPCImageJServerRunnerWithUI typedRunner =
+			(HPCImageJServerRunnerWithUI) runner;
+		typedRunner.initOwnerWindow(ownerWindow);
 	}
 }
